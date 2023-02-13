5076e2f8266dc98ce62a51ea6eeaffdd9c0a963b
wisftock <fullstack.sw@gmail.com>
2023-02-13 13:50:59 -0500

    feat: configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'standard'],
+  overrides: [],
+  parserOptions: {
+    ecmacFacture: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'comma-dangle': [2, 'always-multiline'],
+    semi: 'off',
+    'no-unused-vars': 'warn',
+  },
+};
